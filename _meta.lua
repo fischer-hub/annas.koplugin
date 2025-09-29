c5a4e27c1365336464cab793b4b7820447aab7a9
David Fischer <davidfischer99@gmx.de>
2025-09-29 14:09:01 +0200

    bump version in meta.lua

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "annas",
     fullname = T("Anna's Archive"),
     description = T("[[A plugin to search and download books from Anna's Archive.]]"),
-    version = "0.0.9",
+    version = "0.1.2",
 }
